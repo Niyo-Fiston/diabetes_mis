64af2732707f58463a7236be46ee6d68b365aff3
Fiston NIYOMUTONI <niyomutonifiston@gmail.com>
2024-01-22 04:48:48 +0200

    final commit

--- patients/admin.py
+++ patients/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-'''from .models import Patient
+from .models import Patient
 
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'age', 'glucose_level', 'weight', 'height')
     search_fields = ('first_name', 'last_name', 'age', 'glucose_level', 'weight', 'height')
 
-admin.site.register(Patient, PatientAdmin)'''
+admin.site.register(Patient, PatientAdmin)
+admin.site.site_header = 'Biryogo Diabetes Administration'
+admin.site.site_title = 'Biryogo Diabetes Admin'
+admin.site.index_title = 'Welcome to Biryogo Diabetes Administration'
